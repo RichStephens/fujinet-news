fe51619747ea5c0c98927b0e985c5006893e7802
Rich Stephens <richswww@gmail.com>
2025-08-29 15:42:24 -0400

    Work with 40/80 char modes on Coco3, Add <G>oto page command in article list

--- clients/coco/src/bar.c
+++ clients/coco/src/bar.c
@@ -9,25 +9,62 @@
 #include <cmoc.h>
 #include <coco.h>
 #include "bar.h"
+#include "globals.h"
 
+#define SCREEN_RAM_TOP_32 0x0400
+#define SCREEN_RAM_TOP_40_80 0x6C000
 /**
  * @brief toggle bar for Y position (0-15) 
  * @param y vertical position (0-15) 
  */
 void bar(int y)
 {
-    unsigned char *sp = (unsigned char *)0x0400;
-    int o = y << 5;
+    byte *sp;
+    int o = 0;
 
-    sp += o;
-
-    for (int i=0;i<32;i++)
+    if (textMode == 32)
     {
-        // Only change non-semigraphics characters
-        if (*sp < 0x80)
-            *sp ^= 0x40;
-        sp++;
+        sp = (byte *)SCREEN_RAM_TOP_32;
+        o = y << 5; // Multiply by 32
+        sp += o;
+        // In 32 column mode, the bar is a full line of inverse text
+        for (int i=0;i<32;i++)
+        {
+            // Only change non-semigraphics characters
+            if (*sp < 0x80)
+                *sp ^= 0x40;
+            sp++;
+        }
     }
+}
+
+/**
+ * @brief Draw text with underline on/off at line y
+ * @param y Vertical position (0-23)
+ * @param text The text to print
+ * @param on Underline on/off 
+ */
+void underline(int y, const char *text, BOOL on)
+{
+    locate(0, (byte) y);
+    attr(0,0, FALSE, on);
+    printf("%s", text);
+}
+
+void hd_bar(int y, int fgcolor, int bgcolor, const char *text)
+{
+    locate(0, (byte) y);
+    attr((byte) fgcolor,(byte) bgcolor, FALSE, FALSE);
+    if (textMode == 40)
+    {
+        printf("%-39s", text);
+    }
+    else
+    {
+        printf("%-79s", text);
+    }
+    attr(0,0,FALSE,FALSE);
+    locate(textMode - 1, 23);
 }
 
 /**
@@ -37,11 +74,14 @@ void bar(int y)
  */
 void shadow(int y, int c)
 {
-    unsigned char *sp = (unsigned char *)0x400;
+    byte *sp = (byte *)SCREEN_RAM_TOP_32;
     int o = y << 5;
 
     sp += o;
 
-    *sp = (unsigned char)c | 0x0b;
+    *sp = (byte)c | 0x0b;
     memset(sp+1,c | 0x03, 31);
 }
+
+
+
--- clients/coco/src/globals.h
+++ clients/coco/src/globals.h
@@ -9,6 +9,25 @@
 #ifndef GLOBALS_H
 #define GLOBALS_H
 
+#include <cmoc.h>
+#include <coco.h>
+
+#define true 1
+#define false 0
+
+#define ARROW_UP 0x5E
+#define ARROW_DOWN 0x0A
+#define ARROW_LEFT 0x08
+#define ARROW_RIGHT 0x09
+#define ENTER 0x0D
+#define BREAK 0x03
+
+#define FG_BLACK 0
+#define FG_GREEN 1
+#define BG_GREEN 0
+#define BG_BLUE 2
+#define BG_RED 4
+
 /**
  * @brief available program states
  * @enum TOPICS current topics
@@ -45,5 +64,22 @@ extern const char *urlBase;
  * @brief The strtok() delimiter used for returned article lists
  */
 extern const char *delim;
+
+/**
+ * @brief The text mode - 32, 40, or 80
+ */
+extern unsigned char textMode;
+
+/**
+ * @brief Strings representing the available topics
+ */
+extern const char *topicStrings[];
+
+/**
+ * @brief Determine if string is numeric
+ * @param s The string to test
+ * @return true if the string is numeric, false otherwise
+ */
+BOOL is_numeric(const char *s);
 
 #endif /* GLOBALS_H */
